d6b045812aeecc7a422f5d4d2ab726504c574983
asd0999 <obelixotic@gmail.com>
2019-05-10 11:29:38 -0400

    csv to postgres, potsgrs to p5

--- aptest/index.js
+++ aptest/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
-app.use(express.static('.'));
+app.use(express.static('./wifi-location-tracker/web'));
 
 // var bodyParser = require("body-parser");
 //app.use(bodyParser.json());
+var counter = 0;
 
 var {
   Client
@@ -23,7 +24,6 @@ const PORT = process.env.PORT || 8000;
 var fs = require('fs');
 var parse = require('csv-parse');
 
-var counter = 0;
 
 var csvData = [];
 fs.createReadStream('aptest-02.csv')
@@ -31,26 +31,48 @@ fs.createReadStream('aptest-02.csv')
     delimiter: ';'
   }))
   .on('data', function(csvrow) {
-    console.log(csvrow);
-    //do something with csvrow
+    console.log(csvrow); //print each row of csv file
+    // counter = 0;
     if (counter > 4) {
       csvData.push(csvrow);
     }
     counter++;
+    client.query('DELETE FROM devices', function(e, r) {
+      console.log('deleting old entries');
+    });
+
   })
   .on('end', function() {
-    //do something wiht csvData
     console.log('new csv');
-    console.log(csvData);
+    console.log(csvData); //print each row of new csv file excluding the first 4 rows
+
     csvData.forEach(element => {
-      // console.log(typeof(element[0]));
-      console.log(element[0]);
       let arr = element[0].split(',');
       // console.log(arr[0]);
-      client.query('INSERT INTO devices (station, ap) VALUES ($1, $2)', [arr[0], arr[5]], function(error, results) {});
-
+      client.query('INSERT INTO devices (station, ap) VALUES ($1, $2)', [arr[0], arr[5]], function(error, results) {
+        console.log('adding new entries');
+      });
     });
+    // client.query('DELETE FROM devices ORDER BY id DESC LIMIT 1', function(error, results) {
+    //   console.log('deleted last blank entry');
+    // });
   });
+
+app.get('/getData', function(req, res1) {
+  let message = [];
+  // console.log("got data request from main.js");
+  client.query('SELECT * FROM devices', function(err, result) {
+    if (err) {
+      console.log(error);
+    }
+    for (let row of result.rows) {
+      message.push(row);
+    }
+    // console.log(message);
+    res1.send(message);
+  });
+});
+
 
 app.listen(PORT, function() {
   console.log('Server up! Listening on port: ' + PORT);
